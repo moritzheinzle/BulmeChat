6369f5951db24a92040e511b55ea1da513020eb9
Heinzle Moritz <moritz.heinzle@bulme.at>
2024-09-27 17:20:22 +0200

    Added send and receive func

--- client.py
+++ client.py
@@ -6,3 +6,16 @@ from tkinter import *
 host = "10.0.0.143"
 port = "312"
 
+def receive():
+    while True:
+        try:
+            msg = s.recv(1024).decode()
+            msg_list.insert(tk.end)
+        except Exception:
+            print("There is an Error Receiving Message")
+
+def send():
+    msg = my_msg.get()
+    my_msg.set("")
+    s.send(bytes(msg, "utf8"))
+
