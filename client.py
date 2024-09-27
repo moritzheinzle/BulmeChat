863415a89e6941153540e00035b5779d966d84b2
Heinzle Moritz <moritz.heinzle@bulme.at>
2024-09-27 17:20:43 +0200

    Added GUI and socket

--- client.py
+++ client.py
@@ -19,3 +19,32 @@ def send():
     my_msg.set("")
     s.send(bytes(msg, "utf8"))
 
+window = Tk()
+window.title("Chat Application")
+window.configure(bg="green")
+
+message_frame = Frame(window, height=100, width=100, bg='white')
+message_frame.pack()
+
+my_msg = StringVar()
+my_msg.set("")
+
+scroll_bar = Scrollbar(message_frame)
+msg_list = Listbox(message_frame, height=15, width=100, bg="white", yscrollcommand=scroll_bar.set)
+scroll_bar.pack(side=RIGHT, fill=Y)
+msg_list.pack(side=LEFT, fill=BOTH)
+msg_list.pack()
+
+entry_field = Entry(window, textvariable=my_msg, fg="black", width=50)
+entry_field.pack()
+
+send_button = Button(window, text="Send", font="Aerial", fg="black", bg="blue", command=send)
+send_button.pack()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+
+mainloop()
